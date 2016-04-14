9f2c29759b61e0cc0c7765268d47478c55a0ccf5
Cross_caiex_could1 <caiex@could1>
2016-04-14 23:52:26 +0800

    correct: wrong conf

--- 7001/start.sh
+++ 7001/start.sh
@@ -1,5 +1,5 @@
 supervisord
-sleep 3
+sleep 1
 
 #IP=`ifconfig | grep "inet addr:17" | cut -f2 -d ":" | cut -f1 -d " "`
 #echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 1 ${IP}:7000 ${IP}:7001 ${IP}:7002 ${IP}:7003 ${IP}:7004 ${IP}:7005
--- 7008/dockerbuild.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build -t rediscluster-7008 .
